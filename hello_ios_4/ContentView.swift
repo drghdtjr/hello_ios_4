11728fb4c69f8767c00fde10430d8fc94437c957
kimhongseok <drghdtjr@naver.com>
2024-01-31 20:36:30 +0900

    24.01.31

--- hello_ios_4/ContentView.swift
+++ hello_ios_4/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playercard="card7"
+    @State var cpucard="card2"
+    @State var playerscore=0
+    @State var cpuscore=0
     var body: some View {
         ZStack{
             Image("background-plain")
@@ -17,20 +21,26 @@ struct ContentView: View {
                 Image("logo")
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playercard)
                     Spacer()
-                    Image("card3")
+                    Image(cpucard)
                     Spacer()
                 }
                 
-                Image("button")
+                Button{
+                    deal()
+                }label: {
+                    Image("button")
+                    
+                }
+                
                 HStack{
                     Spacer()
                     VStack{
                         Text("player")
                             .font(.headline)
                             .padding(.bottom,10.0)
-                        Text("0")
+                        Text(String(playerscore))
                             .font(.largeTitle)
                     }
                     .foregroundColor(.white)
@@ -39,7 +49,7 @@ struct ContentView: View {
                         Text("cpu")
                             .font(.headline)
                             .padding(.bottom,10.0)
-                        Text("0")
+                        Text(String(cpuscore))
                             .font(.largeTitle)
                     }
                     .foregroundColor(.white)
@@ -53,6 +63,21 @@ struct ContentView: View {
         
         
     }
+    
+    func deal(){
+        var player_num=Int.random(in: 2...14)
+        playercard="card"+String(player_num)
+        var cpu_num=Int.random(in: 2...14)
+        cpucard="card"+String(cpu_num)
+        if(player_num>cpu_num){
+            playerscore+=1
+        }
+        else if(player_num<cpu_num){
+            cpuscore+=1
+        }
+        
+    }
+    
 }
 
 #Preview {
